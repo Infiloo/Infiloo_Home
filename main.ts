baf267d74a7a7b28186c3491f31736c33399e8b4
Infiloo <67906754+Infiloo@users.noreply.github.com>
2022-05-26 10:53:11 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,275 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
+    images.createBigImage(`
+        . . . . # # . . . .
+        . . . # . . # . . .
+        . . # . . . . # . .
+        . # . . . . . . # .
+        # . . . . . . . . #
+        `).scrollImage(1, 200)
+    images.createBigImage(`
+        . . . . # # . . . .
+        . . . # . . # . . .
+        . . # . . . . # . .
+        . # . . . . . . # .
+        # . . . . . . . . #
+        `).scrollImage(1, 200)
+    images.createBigImage(`
+        . . . . # # . . . .
+        . . . # . . # . . .
+        . . # . . . . # . .
+        . # . . . . . . # .
+        # . . . . . . . . #
+        `).scrollImage(1, 200)
+    images.createBigImage(`
+        . . . . # # . . . .
+        . . . # . . # . . .
+        . . # . . . . # . .
+        . # . . . . . . # .
+        # . . . . . . . . #
+        `).scrollImage(1, 200)
+    images.createBigImage(`
+        . . . . # # . . . .
+        . . . # . . # . . .
+        . . # . . . . # . .
+        . # . . . . . . # .
+        # . . . . . . . . #
+        `).scrollImage(1, 200)
+    images.createBigImage(`
+        . . . . # # . . . .
+        . . . # . . # . . .
+        . . # . . . . # . .
+        . # . . . . . . # .
+        # . . . . . . . . #
+        `).scrollImage(1, 200)
+    basic.showString("" + (input.temperature()))
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . # # # #
+        . # . . #
+        . # . # #
+        # # . # #
+        # # . . .
+        `)
+    images.createBigImage(`
+        . . . . # # . . . .
+        . . . # . . # . . .
+        . . # . . . . # . .
+        . # . . . . . . # .
+        # . . . . . . . . #
+        `).scrollImage(1, 200)
+    images.createBigImage(`
+        . . . . # # . . . .
+        . . . # . . # . . .
+        . . # . . . . # . .
+        . # . . . . . . # .
+        # . . . . . . . . #
+        `).scrollImage(1, 200)
+    images.createBigImage(`
+        . . . . # # . . . .
+        . . . # . . # . . .
+        . . # . . . . # . .
+        . # . . . . . . # .
+        # . . . . . . . . #
+        `).scrollImage(1, 200)
+    images.createBigImage(`
+        . . . . # # . . . .
+        . . . # . . # . . .
+        . . # . . . . # . .
+        . # . . . . . . # .
+        # . . . . . . . . #
+        `).scrollImage(1, 200)
+    images.createBigImage(`
+        . . . . # # . . . .
+        . . . # . . # . . .
+        . . # . . . . # . .
+        . # . . . . . . # .
+        # . . . . . . . . #
+        `).scrollImage(1, 200)
+    images.createBigImage(`
+        . . . . # # . . . .
+        . . . # . . # . . .
+        . . # . . . . # . .
+        . # . . . . . . # .
+        # . . . . . . . . #
+        `).scrollImage(1, 200)
+    music.startMelody(music.builtInMelody(Melodies.Nyan), MelodyOptions.Once)
+    images.createBigImage(`
+        . . . . . . . . . #
+        . . . . . . . . . #
+        . . . . # # . . # #
+        . # . # # # # . # #
+        # # # # # # # # # #
+        `).scrollImage(1, 200)
+    images.createBigImage(`
+        . . . . . . . . . #
+        . . . . . . . . . #
+        . . . . # # . . # #
+        . # . # # # # . # #
+        # # # # # # # # # #
+        `).scrollImage(1, 200)
+    images.createBigImage(`
+        . . . . . . . . . #
+        . . . . . . . . . #
+        . . . . # # . . # #
+        . # . # # # # . # #
+        # # # # # # # # # #
+        `).scrollImage(1, 200)
+    images.createBigImage(`
+        . . . . . . . . . #
+        . . . . . . . . . #
+        . . . . # # . . # #
+        . # . # # # # . # #
+        # # # # # # # # # #
+        `).scrollImage(1, 200)
+    images.createBigImage(`
+        . . . . . . . . . #
+        . . . . . . . . . #
+        . . . . # # . . # #
+        . # . # # # # . # #
+        # # # # # # # # # #
+        `).scrollImage(1, 200)
+    images.createBigImage(`
+        . . . . . . . . . #
+        . . . . . . . . . #
+        . . . . # # . . # #
+        . # . # # # # . # #
+        # # # # # # # # # #
+        `).scrollImage(1, 200)
+    images.createBigImage(`
+        . . . . . . . . . #
+        . . . . . . . . . #
+        . . . . # # . . # #
+        . # . # # # # . # #
+        # # # # # # # # # #
+        `).scrollImage(1, 200)
+    images.createBigImage(`
+        . . . . . . . . . #
+        . . . . . . . . . #
+        . . . . # # . . # #
+        . # . # # # # . # #
+        # # # # # # # # # #
+        `).scrollImage(1, 200)
+    images.createBigImage(`
+        . . . . . . . . . #
+        . . . . . . . . . #
+        . . . . # # . . # #
+        . # . # # # # . # #
+        # # # # # # # # # #
+        `).scrollImage(1, 200)
+    music.startMelody(music.builtInMelody(Melodies.Chase), MelodyOptions.Once)
+    images.createBigImage(`
+        . . . . . . . . . #
+        . . . . . . . . . #
+        . . . . # # . . # #
+        . # . # # # # . # #
+        # # # # # # # # # #
+        `).scrollImage(1, 200)
+    images.createBigImage(`
+        . . . . . . . . . #
+        . . . . . . . . . #
+        . . . . # # . . # #
+        . # . # # # # . # #
+        # # # # # # # # # #
+        `).scrollImage(1, 200)
+    images.createBigImage(`
+        . . . . . . . . . #
+        . . . . . . . . . #
+        . . . . # # . . # #
+        . # . # # # # . # #
+        # # # # # # # # # #
+        `).scrollImage(1, 200)
+    images.createBigImage(`
+        . . . . . . . . . #
+        . . . . . . . . . #
+        . . . . # # . . # #
+        . # . # # # # . # #
+        # # # # # # # # # #
+        `).scrollImage(1, 200)
+    images.createBigImage(`
+        . . . . . . . . . #
+        . . . . . . . . . #
+        . . . . # # . . # #
+        . # . # # # # . # #
+        # # # # # # # # # #
+        `).scrollImage(1, 200)
+    images.createBigImage(`
+        . . . . . . . . . #
+        . . . . . . . . . #
+        . . . . # # . . # #
+        . # . # # # # . # #
+        # # # # # # # # # #
+        `).scrollImage(1, 200)
+    images.createBigImage(`
+        . . . . . . . . . #
+        . . . . . . . . . #
+        . . . . # # . . # #
+        . # . # # # # . # #
+        # # # # # # # # # #
+        `).scrollImage(1, 200)
+    images.createBigImage(`
+        . . . . . . . . . #
+        . . . . . . . . . #
+        . . . . # # . . # #
+        . # . # # # # . # #
+        # # # # # # # # # #
+        `).scrollImage(1, 200)
+    images.createBigImage(`
+        . . . . . . . . . #
+        . . . . . . . . . #
+        . . . . # # . . # #
+        . # . # # # # . # #
+        # # # # # # # # # #
+        `).scrollImage(1, 200)
+    images.createBigImage(`
+        . . . . . . . . . #
+        . . . . . . . . . #
+        . . . . # # . . # #
+        . # . # # # # . # #
+        # # # # # # # # # #
+        `).scrollImage(1, 200)
+})
+music.setBuiltInSpeakerEnabled(true)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    # . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.showLeds(`
+    . . . . .
+    . # . . .
+    # . . . .
+    . # . . .
+    . . . . .
+    `)
+basic.showLeds(`
+    . . . . .
+    . # . . .
+    # . # . .
+    . # . . .
+    . . . . .
+    `)
+basic.showLeds(`
+    . . . . .
+    . # . # .
+    # . # . .
+    . # . # .
+    . . . . .
+    `)
+basic.showLeds(`
+    . . . . .
+    . # . # .
+    # . # . #
+    . # . # .
+    . . . . .
+    `)
+music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Once)
